6151b7e6867f0912d03117020cbed647d977235a
adarsh-gupta-2002 <111111623+adarsh-gupta-2002@users.noreply.github.com>
2023-02-21 13:16:33 +0530

    initial commit

--- js/index.js
+++ js/index.js
@@ -4,12 +4,25 @@ const menuOpen = document.getElementById('menu-open')
 const menuClose = document.getElementById('menu-close')
 
 const nav = document.querySelector('.nav')
-const dropDown = document.querySelectorAll('.drop-down')
-
+ 
+ 
 menuOpen.addEventListener('click' , () =>{
     nav.style.display = 'block'
 })
 menuClose.addEventListener('click' , () =>{
-    nav.style.display = 'none'
+   
+
+        nav.style.display = 'none'
+    
 })
- 
+
+
+window.addEventListener('resize' , () => {
+    if(screen.width > 800){
+        nav.style.display = 'block'
+    }else{
+        nav.style.display = 'none'
+    }
+    
+    
+})
